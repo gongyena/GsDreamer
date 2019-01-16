625a4da6c643c954f77f3185e6bef9d8eebc8b08
gongyena <39291723+gongyena@users.noreply.github.com>
2019-01-16 17:05:08 +0900

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <center><a href="first.php"><img src="one.PNG" width="200" height="200" border="0"></a>    
   <a href="second.php"><img src="two.PNG" width="200" height="200" border="0"></a>     
   <a href="third.php"><img src="three.PNG" width="200" height="200" border="0"></a></center><br>
-<center><a href="fourth.phpl"><img src="four.PNG" width="200" height="200"  border="0"></a>     
+<center><a href="fourth.php"><img src="four.PNG" width="200" height="200"  border="0"></a>     
   <a href="fifth.php"><img src="five.PNG" width="200" height="200" border="0"></a>     
   <a href="sixth.php"><img src="six.PNG" width="200" height="200" border="0"></a>     </center><br>
 <!--"C:\Users\SAMSUNG\Desktop\hightech_img\two.PNG"는 이재인 컴퓨터에서만 실행되는거 같아서 걍 하위폴더에서 찾아쓰도록 만들었음. (아마?) -->
