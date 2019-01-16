6c4912ba7f35c4a4df4f7a113062ce6295ca3c2a
gongyena <39291723+gongyena@users.noreply.github.com>
2019-01-16 16:09:17 +0900

    Create edit_proc1.php

---
+++ edit_proc1.php
@@ -0,0 +1,16 @@
+<?PHP
+include "php/dbcon1.php";
+$subject = $_POST["subject"];
+$pwd = $_POST["pwd"];
+$content = $_POST["content"];
+$num = $_POST["num"];
+$sql = "UPDATE board SET subject = '$subject', pwd='$pwd', content='$content' WHERE num = '$num'";
+if (mysqli_query($conn, $sql)) {
+	echo("<script>location.replace('first.html');</script>");
+		mysqli_close($conn);
+} else {
+	echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		mysqli_close($conn);
+}
+mysqli_close($conn);
+?>
