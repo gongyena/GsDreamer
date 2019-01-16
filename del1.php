419b7c16fe44cc01cd697a909531771a068c2156
gongyena <39291723+gongyena@users.noreply.github.com>
2019-01-16 16:06:39 +0900

    Create del1.php

---
+++ del1.php
@@ -0,0 +1,10 @@
+<?php
+include "php/dbcon1.php";
+$num = $_GET["num"];  //넘어온 인수(글번호)
+$sql = "DELETE FROM board WHERE num=$num";
+mysqli_query($conn, $sql);
+mysqli_close($conn);
+?>
+<script>
+		location.replace('first.html');
+</script>
