83632d54496cb38c9370662443f4f1877035ac33
gongyena <39291723+gongyena@users.noreply.github.com>
2019-01-16 16:39:56 +0900

    Update and rename edit_proc1.html to edit_proc1.php

--- edit_proc1.php
+++ edit_proc1.php
@@ -1,12 +1,12 @@
 <?PHP
-include "dbcon1.html";
+include "dbcon1.php";
 $subject = $_POST["subject"];
 $pwd = $_POST["pwd"];
 $content = $_POST["content"];
 $num = $_POST["num"];
 $sql = "UPDATE board SET subject = '$subject', pwd='$pwd', content='$content' WHERE num = '$num'";
 if (mysqli_query($conn, $sql)) {
-	echo("<script>location.replace('first.html');</script>");
+	echo("<script>location.replace('first.php');</script>");
 		mysqli_close($conn);
 } else {
 	echo "Error: " . $sql . "<br>" . mysqli_error($conn);
